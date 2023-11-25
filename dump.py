eaae430d3756ad433b0030d0c2c2eed7e2f92d68
DetrembleurArthur <detrembleur.arthur@gmail.com>
2023-11-25 23:33:37 +0100

    clear json

--- dump.py
+++ dump.py
@@ -2,7 +2,7 @@ import requests
 import sys
 import json
 
-import pymongo
+"""import pymongo
 
 mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
 halodb = mongo_client["halodb"]
@@ -27,6 +27,26 @@ def dump_games(pseudo):
 		offset += 25
 		print(f"{pseudo} games dumped: {offset}")
 	print("games dumped into mongodb")
+"""
+
+
+def dump_games(pseudo):
+	print(f"dump {pseudo} games")
+	games = []
+	offset = 0
+	while True:
+		url = f"https://sr-nextjs.vercel.app/api/halodotapi?path=%2Fgames%2Fhalo-infinite%2Fstats%2Fmultiplayer%2Fplayers%2F{pseudo}%2Fmatches%3Ftype%3Dmatchmaking%26count%3D25%26offset%3D{offset}"
+		result = requests.get(url)
+		result = result.json()["data"]
+		if len(result) == 0:
+			break
+		games.append(result)
+		offset += 25
+		print(f"{pseudo} games dumped: {offset}")
+	print("saving")
+	with open(f"{pseudo}_games.json", "wt") as file:
+		file.write(json.dumps({f"games": games}, indent=4))
+	print("games saved")
 
 if __name__ == "__main__":
 	dump_games(sys.argv[1])
